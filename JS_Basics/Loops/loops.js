b59f7a6becb19205e07784029467b64d0f882d2c
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-08-05 11:16:08 +0530

    loops done

---
+++ JS_Basics/Loops/loops.js
@@ -0,0 +1,33 @@
+// print 1-50 numbers
+// for (let i=1; i<=50; i++){
+//     console.log(i)
+// }
+
+// print sum of 1-20 using while
+// let sum = 0;
+// let i = 1
+// while(i<=10){
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum)
+
+// print characters of string
+//    const printChar= (str) => {
+//     for (let i = 0; i < str.length; i++) {
+//         console.log(str[i])
+//     }
+//    } 
+// printChar("abc")
+// printChar("Javascript")
+
+// using for of = sirf string pe lagta hai
+const printChar= (str) => {
+    for (let char of str) {
+        console.log(char)
+    }
+   } 
+// printChar("abc")
+// printChar("Javascript")
+printChar("rrrrrr")
+
